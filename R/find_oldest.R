7ee2f1b7f5a532032cf11d4561a2852d4e50c578
mikedparrott <mikedparrott@gmail.com>
2018-02-21 12:15:37 -0500

    Add files via upload

---
+++ R/find_oldest.R
@@ -0,0 +1,20 @@
+#' A Function to Find Oldest NBA Player by Year
+#'
+#' This function allows you to Find Oldest NBA Player by Year
+#' @param yearinput
+#' @keywords nba,oldest
+#' @export
+#' @examples
+#' find_oldest()
+
+find_oldest<-function(yearinput){
+
+ageyeardata<-  dplyr::select(nba_data, Year, Age)
+ yeardata<- dplyr::filter(ageyeardata,Year==yearinput)
+yeardata<-yeardata[complete.cases(yeardata),] #remove missing values NA, NaN, and Inf values
+
+ max(yeardata$Age,na.rm=TRUE)
+
+
+}
+
